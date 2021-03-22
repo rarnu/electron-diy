57eb4ca378e415ca61c331a539bccf0ab1dbe910
hexiaojie <hexj@isyscore.com>
2021-03-22 19:00:17 +0800

    add: new js

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const {app, BrowserWindow, Menu, ipcMain, dialog, nativeTheme} = require('electron');
 const path = require('path');
-const fs = require("fs");
-const moment = require("moment");
+const fs = require('fs');
+const request = require('request');
+const md5 = require('md5-node');
 
 let window = null;
 let USER_HOME = process.env.HOME;
 let SPLIT = '/';
+
+const appid = '20171201000101453';
+const secret = 'MLhIYijr23XUvrDMxOwS';
 
 function createWindow() {
     Menu.setApplicationMenu(null);
@@ -20,7 +24,6 @@ function createWindow() {
             enableRemoteModule: true,
             preload: path.join(__dirname, 'preload.js')
         },
-
         icon: path.join(__dirname, 'icon.png')
     })
     // 针对 mac 特殊处理
@@ -65,21 +68,41 @@ ipcMain.on('capture', (event, args) => {
             defaultPath: `${USER_HOME}${SPLIT}${args.name}.png`
         }).then((result) => {
             if (!result.canceled) {
-                fs.writeFile(result.filePath, base64Data, 'base64', function (err) {
+                fs.writeFile(result.filePath, base64Data, 'base64', (err) => {
 
                 });
             }
         })
     });
-
 });
 
-function captureCard() {
-    window.capturePage({x: 20, y: 20, width: 697, height: 1016}).then((img) => {
-        let b64 = img.toDataURL();
-        const base64Data = b64.replace(/^data:image\/png;base64,/, "");
-        fs.writeFile(`${__dirname}/example.png`, base64Data, 'base64', function (err) {
-            console.log(err);
-        });
-    });
+/* 接入百度翻译 */
+
+function getSalt() {
+    let salt = "";
+    for (let i = 0; i < 10; i++) {
+        salt += Math.floor(Math.random() * 10);
+    }
+    return salt;
 }
+
+ipcMain.on('translate', (event, args) => {
+    console.log('translate');
+    let timestamp = new Date().getTime();
+    let q = args.text;
+    let salt = getSalt();
+    let signStr = `${appid}${q}${salt}${secret}`;
+    let sign = md5(signStr);
+    request.get({
+        url: `http://api.fanyi.baidu.com/api/trans/vip/translate?q=${encodeURI(q)}&from=zh&to=jp&appid=${appid}&salt=${salt}&sign=${sign}`,
+        method: 'get'
+    }, (err, resp, body) => {
+        const ret = JSON.parse(body);
+        let texts = [];
+        ret.trans_result.forEach((item) => {
+            texts.push(item.dst);
+        });
+        event.sender.send('translate-reply', {texts: texts});
+    });
+
+});
